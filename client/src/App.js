fb613852a19234f85601b520181ca0b0c2c54208
Ramsha <ramshanasir.rn@gmail.com>
2020-12-03 12:53:59 -0600

    local

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,27 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useState, useEffect} from 'react'
+
+//development
+//production
+
+function url(path) {
+  return process.env.NODE_ENV === 'development'
+    ? `http://localhost:5000${path}`
+    :''
+}
 
 function App() {
+  const [data, setData] = useState('')
+  useEffect(() => {
+    fetch(url('/api/data'))
+      .then(res => res.json())
+    .then(goods =>setData(goods.data))
+  },[])
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Web 36 rocks</h1>
+        {data}
       </header>
     </div>
   );
